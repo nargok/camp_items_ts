f3ca1b6b3f16c559ab4a9efd4f29623ba233b8a6
nargok <nargok@gmail.com>
2020-08-30 18:00:37 +0900

    hello component

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./Hello";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Hello message="Hello React with TypeScript" />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Hello.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface HelloProps {
+  message?: string;
+}
+
+const Hello: React.FC<HelloProps> = ({ message }) => {
+  return <div>{message}</div>;
+};
+
+export default Hello;
